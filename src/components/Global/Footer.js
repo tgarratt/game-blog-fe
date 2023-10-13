9f967f811a984f3b07e2dd05d58e6701b5e29430
tgarratt <tomagarratt@yahoo.co.uk>
2023-10-13 21:33:29 +0100

    improved footer padding

--- src/components/Global/Footer.js
+++ src/components/Global/Footer.js
@@ -17,38 +17,40 @@ function Footer() {
     <>
         <div className='colored-seperator'  />
         <footer className='secondary-block'>
-        <div className='flex flex-col'>
-          <FooterElement>
-            <p className='text my-1 cursor-pointer w-fit'>
-              <a href="/about">About</a>
-            </p>
-          </FooterElement>
-          <FooterElement>
-            <p className='text my-1 cursor-pointer w-fit'>
-              <a href="/reviews">All Reviews</a>
-            </p>
-          </FooterElement>
-          <FooterElement>
-            <p className='text my-1 cursor-pointer w-fit'>
-              <a target="_blank" rel="noreferrer" href="https://github.com/tgarratt">Github</a>
-            </p>
-          </FooterElement>  
-          <FooterElement>
-            <p className='text my-1 cursor-pointer w-fit'>
-              <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tom-a-garratt/">LinkedIn</a>
-            </p>
-          </FooterElement>    
-          <FooterElement>
-            <p className='text my-1 cursor-pointer w-fit'>
-              <a href="mailto:tomagarratt@yahoo.co.uk">tomagarratt@yahoo.co.uk</a>
-            </p>
-          </FooterElement>      
-       </div>
-       <div className='flex justify-end items-center mt-4 md:mt-0'>
-          <div className='h-[2rem]'>
-            <Logo />
-          </div>  
-          <div className='text pl-4 text-3xl heading'>TechTom Plays</div>
+          <div className='py-1'>
+            <div className='flex flex-col'>
+              <FooterElement>
+                <p className='text my-1 cursor-pointer w-fit'>
+                  <a href="/about">About</a>
+                </p>
+              </FooterElement>
+              <FooterElement>
+                <p className='text my-1 cursor-pointer w-fit'>
+                  <a href="/reviews">All Reviews</a>
+                </p>
+              </FooterElement>
+              <FooterElement>
+                <p className='text my-1 cursor-pointer w-fit'>
+                  <a target="_blank" rel="noreferrer" href="https://github.com/tgarratt">Github</a>
+                </p>
+              </FooterElement>  
+              <FooterElement>
+                <p className='text my-1 cursor-pointer w-fit'>
+                  <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/tom-a-garratt/">LinkedIn</a>
+                </p>
+              </FooterElement>    
+              <FooterElement>
+                <p className='text my-1 cursor-pointer w-fit'>
+                  <a href="mailto:tomagarratt@yahoo.co.uk">tomagarratt@yahoo.co.uk</a>
+                </p>
+              </FooterElement>      
+          </div>
+          <div className='flex justify-end items-center mt-4 md:mt-0'>
+            <div className='h-[2rem]'>
+              <Logo />
+            </div>  
+            <div className='text pl-4 text-3xl heading'>TechTom Plays</div>
+          </div>
         </div>
       </footer>
     </>
